1b3afbd6f35e5a0770ec13bae3a5baf4e8525ac5
Cristian Bica <cristian.bica@gmail.com>
2016-12-22 11:06:43 +0200

    Removed hardcoded client_assertion and use client_secret

--- lib/omniauth/strategies/vsts.rb
+++ lib/omniauth/strategies/vsts.rb
@@ -7,7 +7,6 @@ module OmniAuth
       option :authorize_params, {:response_type => 'Assertion'}
       option :token_params, {
         :client_assertion_type => 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-        :client_assertion => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im9PdmN6NU1fN3AtSGpJS2xGWHo5M3VfVjBabyJ9.eyJjaWQiOiJjZmI4N2U1MS00NGJlLTRlZjEtOWUxNC02OTVlZGZjODFiOTciLCJjc2kiOiIyYzUyMDU2My0zMjhiLTQzMGMtOGUzNy0wN2NhODg2ZjgxOWEiLCJuYW1laWQiOiIyNzk2ZjU2Ni03OWUzLTQ3YmItOTU5Ny1kMzFlYmM5MDU3ZDMiLCJpc3MiOiJhcHAudnNzcHMudmlzdWFsc3R1ZGlvLmNvbSIsImF1ZCI6ImFwcC52c3Nwcy52aXN1YWxzdHVkaW8uY29tIiwibmJmIjoxNDcyMTI4NzEzLCJleHAiOjE2Mjk4OTUxMTN9.dMBpndiMeePFCkZeszncx0_kTjxv5PhhLGwklUZ4Wngtg9v7D30YfRyVERlPfYs76ZC4hgXlmd47Aaw3ZefvIoK6cGfyacpBoQf2df_Ac9qoAgEPyUw_WvgmeBmdsGlJ0Ubs-OOz9-AgESGiuCAABNk1oCDM8AlPlMYprL2Dcz7M0bzS1yIgTYc06YJqJGDoEqkL7Zbgx2wKOb9tMQ92bUq433KhxPW8t43A0dai2jm0XSRzWyxHgiCfyJ9q-J0GHXsNATTuzGAbt8ArkWSQrx7rCIBdKprWpkO8NCpPHp0pJLeAnKxk-ahp4RnuJhLkRU3RKfQBrvHSZl-3Rlis3Q',
         :grant_type => 'urn:ietf:params:oauth:grant-type:jwt-bearer'
       }
 
@@ -53,7 +52,7 @@ module OmniAuth
 
       def build_access_token
         assertion = request.params["code"]
-        client.get_token({:assertion => assertion, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)))
+        client.get_token({:assertion => assertion, client_assertion => options.client_secret, :redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)))
       end
     end
   end
